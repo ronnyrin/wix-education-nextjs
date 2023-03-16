e8155714edb22556550d3025a1013f5215f5756d
ronnyr <ronnyr@wix.com>
2023-03-16 14:15:00 +0200

    contact page

--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -7,21 +7,21 @@ export const CarouselClient = () => {
       <Carousel>
         <div className="flex flex-col gap-14 h-full items-center justify-center bg-blue-site text-white">
           <h3 className="text-4xl max-w-3xl font-site">
-            “I'm a testimonial. Click to edit me and add text that says
+            “I am a testimonial. Click to edit me and add text that says
             something nice about you and your services.”
           </h3>
           <p>Nora Jacobs, Teacher</p>
         </div>
         <div className="flex flex-col gap-14 h-full items-center justify-center bg-blue-site text-white">
           <h3 className="text-4xl max-w-3xl font-site">
-            “I'm a testimonial. Click to edit me and add text that says
+            “I am a testimonial. Click to edit me and add text that says
             something nice about you and your services.”
           </h3>
           <p>Bert Brian, Student</p>
         </div>
         <div className="flex flex-col gap-14 h-full items-center justify-center bg-blue-site text-white">
           <h3 className="text-4xl max-w-3xl font-site">
-            “I'm a testimonial. Click to edit me and add text that says
+            “I am a testimonial. Click to edit me and add text that says
             something nice about you and your services.”
           </h3>
           <p>Lakshmi Samson, Parent</p>
